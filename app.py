a221807cdb6636d6394efdeb3825a02a017665d6
Marco Delvecchio <marco@marcbook-8.clik.polito.it>
2018-12-12 13:40:38 +0100

    Added add_user function that works with cow-db

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import os, time
-import requests
+import time, requests, json
 #import webbrowser
 
 def ing():
-    try:
+    try:            #DOESN'T WORK
         #webbrowser.open('http://origano.clik.polito.it:8888/api/v1/users/1/56/products')
         r = requests.get("http://origano.clik.polito.it:8888/api/v1/users/1/56/products")   #tutti gli ingredienti di user1 scanner56
-        for i in r:
-            print(i, end="\n\n")
+        r = (r.json())
+        print(r)
+        #for i in r:
+            #json.loads('r')
+            #json.loads(r)
+            #print(i)
     except:
         print("Error on API request.")
 
@@ -19,12 +22,56 @@ def sen():
     except:
         print("Error on API request.")
 
+def add():
+    data = {}
+    print ("Welcome!\nPlease insert your email:")
+    data["email"]=input()
+    print ("\nWhat is your Username?")
+    data["username"]=input()
+    print ("\nChoose your password:")
+    data["password"] = input()
+    print ("\nWhat is your name?")
+    data["name"] = input()
+    print ("\nWhat is your surname?")
+    data["surname"] = input()
+    print ("\nWhen were you born? Please insert into yyyy-mm-dd format:")
+    data["birthday"] = input()
+    print ("\nMale or Female? m/f")
+    data ["gender"] = input()
+    print ("\nAlmost done! Where do you live now?")
+    data ["city"] = input()
+    print ("\nWhat is your address?")
+    data ["address"] = input()
+    print ("\nWhat is your postal code?")
+    data ["postalCode"] = input()
+    print ("Thank you for your time! Adding user...\n")
+    #print (data) test
+    json_data=json.dumps(data)
+    #print(json_data) test
+    try:
+        r = requests.post("http://origano.clik.polito.it:8888/api/v1/users", data=json_data)
+        print(r.status_code, r.reason)
+    except:
+        print("Error on API request.")
+
+
 while(1):
-    print ("Premi:\n    i per vedere gli ingredienti;\n    s per vedere i valori dei sensori;\n")
+    print ("Press:\n    i to see all your ingredients;\n    s to see your fridge state;\n    a to add a new user;\n")
     a = input()
+    # def switch(a):
+    #     switcher = {
+    #         0: "ing",
+    #         1: "sen",
+    #         2: "add",
+    #     }
+    #     print ("Invalid button!")
+    #     return switcher.get(a, "nothing")
+
     if a == "i":
         ing()
     elif a == "s":
         sen()
+    elif a == "a":
+        add()
     else:
         print ("Try again! You have pushed the wrong button\n")
